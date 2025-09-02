587ae5819a14d29ba13578a70092837447d099bd
swagatobhaskar <bhaskarswagato@gmail.com>
2025-09-03 02:00:56 +0530

    EDIT: ButtonPrimary which wraps over shadcn Button, now forwards refs.

--- blog-ui/components/ui/button-primary.tsx
+++ blog-ui/components/ui/button-primary.tsx
@@ -1,21 +1,41 @@
 "use client"
 
+import * as React from "react"
 import { Button } from "./button";
 
 type ButtonPrimaryProps = {
     text: string;
-    onclick?: () => void;
+    // onclick?: () => void;
     icon?: React.ReactNode;
-}
+ }& React.ComponentPropsWithoutRef<"button">
 
-export default function ButtonPrimary({text, onclick, icon}: ButtonPrimaryProps) {
+// export default function ButtonPrimary({text, onclick, icon}: ButtonPrimaryProps) {
+//     return (
+//         <Button
+//             className="bg-blue-300 text-white px-4 py-2 rounded cursor-pointer hover:bg-blue-400 mt-4"
+//             onClick={onclick}
+//         >
+//             {icon && <span className="icon">{icon}</span>}
+//             {text}
+//         </Button>
+//     );
+// }
+
+const ButtonPrimary = React.forwardRef<HTMLButtonElement, ButtonPrimaryProps>(
+  ({ text, icon, ...props }, ref) => {
     return (
-        <Button
-            className="bg-blue-300 text-white px-4 py-2 rounded cursor-pointer hover:bg-blue-400 mt-4"
-            onClick={onclick}
-        >
-            {icon && <span className="icon">{icon}</span>}
-            {text}
-        </Button>
-    );
-}
+      <Button
+        ref={ref}
+        className="bg-blue-300 text-white px-4 py-2 rounded cursor-pointer hover:bg-blue-400 mt-4"
+        {...props}
+      >
+        {icon && <span className="icon">{icon}</span>}
+        {text}
+      </Button>
+    )
+  }
+)
+
+ButtonPrimary.displayName = "ButtonPrimary"
+
+export default ButtonPrimary
