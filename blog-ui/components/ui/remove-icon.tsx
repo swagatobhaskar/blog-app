1553dec1cfd84f3394f9977867ae0661218ae2f2
swagatobhaskar <bhaskarswagato@gmail.com>
2025-09-04 02:00:52 +0530

    ADD: reusable SVG remove button component

---
+++ blog-ui/components/ui/remove-icon.tsx
@@ -0,0 +1,36 @@
+type RemoveIconProps = {
+  strokeWidth?: number
+  className?: string
+  color?: string
+  size?: number
+  onClick?: () => void
+}
+
+const RemoveIcon: React.FC<RemoveIconProps> = ({
+    strokeWidth = 1,
+    color = '#000000',
+    size = 18,
+    className = '',
+    onClick,
+}) => (
+  <svg
+    xmlns="http://www.w3.org/2000/svg"
+    width={size}
+    height={size}
+    viewBox="0 0 24 24"
+    fill="none"
+    stroke={color}
+    strokeWidth={strokeWidth}
+    strokeLinecap="round"
+    strokeLinejoin="round"
+    className={className}
+    onClick={onClick}
+  >
+    <polyline points="3 6 5 6 21 6"></polyline>
+    <path d="M19 6v14a2 2 0 0 1-2 2H7a2 2 0 0 1-2-2V6m3 0V4a2 2 0 0 1 2-2h4a2 2 0 0 1 2 2v2"></path>
+    <line x1="10" y1="11" x2="10" y2="17"></line>
+    <line x1="14" y1="11" x2="14" y2="17"></line>
+  </svg>
+)
+
+export default RemoveIcon
