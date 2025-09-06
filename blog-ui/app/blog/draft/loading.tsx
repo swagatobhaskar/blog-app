b623f65f0548ff550474d2a0453d80cff2756081
swagatobhaskar <bhaskarswagato@gmail.com>
2025-09-06 23:16:45 +0530

    ADD: skeleton loading page and component

---
+++ blog-ui/app/blog/draft/loading.tsx
@@ -0,0 +1,8 @@
+// Use loading.tsx for server components
+export default function DraftBlogListLoading() {
+    return (
+        <div>
+            <p className="text-green-600 text-xl">Loading...</p>
+        </div>
+    )
+}
