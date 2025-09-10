4f2fc6341e7bd995b05678e8fb81950a96e8e4e7
swagatobhaskar <bhaskarswagato@gmail.com>
2025-09-11 01:34:26 +0530

    EDIT: imports

--- blog-ui/app/page.tsx
+++ blog-ui/app/page.tsx
@@ -1,7 +1,7 @@
 import Blog from "@/lib/types/blog"
 
 import { BLOG_API_URL } from "@/lib/constants/constants"
-import BlogListItem from "@/components/ui/blog/blog_list_item"
+import BlogListItem from "@/components/blog/blog_list_item"
 
 export default async function Home() {
   const res = await fetch(
