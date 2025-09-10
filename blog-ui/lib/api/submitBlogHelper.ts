9dd48d78201ebc6a20f237c98d1f9bd47910d9af
swagatobhaskar <bhaskarswagato@gmail.com>
2025-09-11 01:34:07 +0530

    will be removed

--- blog-ui/lib/api/submitBlogHelper.ts
+++ blog-ui/lib/api/submitBlogHelper.ts
@@ -24,35 +24,35 @@ const SubmitBlog = async ({
     
     const topicIds = topicsToUse.map(topic => topic.id);
 
-    const newBlogBody = {
-        title,
-        content,
-        topic_ids: topicIds,
-        is_draft: isDraft,
-    };
-    console.log("Submitting blog:", newBlogBody)
+    // const newBlogBody = {
+    //     title,
+    //     content,
+    //     topic_ids: topicIds,
+    //     is_draft: isDraft,
+    // };
+    // console.log("Submitting blog:", newBlogBody)
 
-    try {
-        const resp = await fetch(BLOG_API_URL, {
-            method: 'POST',
-            headers: {
-                'Content-Type': 'application/json',
-            },
-            body: JSON.stringify(newBlogBody),
-        });
+    // try {
+    //     const resp = await fetch(BLOG_API_URL, {
+    //         method: 'POST',
+    //         headers: {
+    //             'Content-Type': 'application/json',
+    //         },
+    //         body: JSON.stringify(newBlogBody),
+    //     });
 
-        if (resp.ok) {
-            return (await resp.json())
+    //     if (resp.ok) {
+    //         return (await resp.json())
             
-        } else {
-            const errorText = await resp.text();
-            console.error("Failed to submit blog:", errorText);
-            alert("Error: " + errorText);
-        }
-    } catch (error) {
-        console.error("Network error:", error);
-        alert("Network error. Please try again.");
-    }
+    //     } else {
+    //         const errorText = await resp.text();
+    //         console.error("Failed to submit blog:", errorText);
+    //         alert("Error: " + errorText);
+    //     }
+    // } catch (error) {
+    //     console.error("Network error:", error);
+    //     alert("Network error. Please try again.");
+    // }
 };
 
 export default SubmitBlog
