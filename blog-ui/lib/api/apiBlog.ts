aceca1eb047cc0bb6097c45bec8a7110121f8cf9
swagatobhaskar <bhaskarswagato@gmail.com>
2025-09-12 00:47:48 +0530

    EDIT: modifications

--- blog-ui/lib/api/apiBlog.ts
+++ blog-ui/lib/api/apiBlog.ts
@@ -1,16 +1,39 @@
 import Blog from "../types/blog"
 import { BLOG_API_URL } from "../constants/constants";
 import { apiHandler } from "./apiFetchHandler";
+import Topic from "../types/topic";
 
 export const getAllBlogs = () => apiHandler<Blog[]>(`${BLOG_API_URL}`)
 
 export const getBlogById = (id: string) => apiHandler<Blog>(`${BLOG_API_URL}/${id}`)
 
-export const createBlog = (data: Omit<Blog, 'id'>) => apiHandler<Blog>(
+export const createBlog = async (title: string, content: string, topics: Topic[]) => {
+    const topic_ids: string[] = topics.map(topic => topic.id);
+    apiHandler<Blog>(
     `${BLOG_API_URL}`, {
         method: "POST",
-        body: JSON.stringify(data)
+        body: JSON.stringify({
+            "title": title,
+            "content": content,
+            "is_draft": false,
+            "topic_ids": topic_ids
+        })
     })
+}
+
+export const createBlogAsDraft = async (title: string, content: string, topics: Topic[]) => {
+    const topic_ids: string[] = topics.map(topic => topic.id);
+    apiHandler<Blog>(
+    `${BLOG_API_URL}`, {
+        method: "POST",
+        body: JSON.stringify({
+            "title": title,
+            "content": content,
+            "is_draft": true,
+            "topic_ids": topic_ids
+        })
+    })
+}
 
 export const updateBlog = (id: string, data: Partial<Blog>) => apiHandler<Blog>(
     `${BLOG_API_URL}/${id}`, {
