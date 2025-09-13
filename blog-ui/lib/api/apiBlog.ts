871e782ef4613ae57003199e4f6232771f7e1e82
swagatobhaskar <bhaskarswagato@gmail.com>
2025-09-13 14:34:54 +0530

    ADD: updateBlogNadSaveDraft, updateBlogAndPublish methods

--- blog-ui/lib/api/apiBlog.ts
+++ blog-ui/lib/api/apiBlog.ts
@@ -7,7 +7,7 @@ export const getAllBlogs = () => apiHandler<Blog[]>(`${BLOG_API_URL}`)
 
 export const getBlogById = (id: string) => apiHandler<Blog>(`${BLOG_API_URL}/${id}`)
 
-export const createBlog = async (title: string, content: string, topics: Topic[]) => {
+export const createBlogAndPublish = async (title: string, content: string, topics: Topic[]) => {
     const topic_ids: string[] = topics.map(topic => topic.id);
 
     const response = await apiHandler<Blog>(`${BLOG_API_URL}`, {
@@ -43,11 +43,39 @@ export const updateBlog = (id: string, data: Partial<Blog>) => apiHandler<Blog>(
         body: JSON.stringify(data)
     })
 
-export const deleteBlog = (id: string) =>
-    apiHandler<{ success: boolean }>(
-        `${BLOG_API_URL}/${id}`, {
-            method: 'DELETE'
+export const updateBlogAndSaveDraft = async (id: string, title: string, content: string, topics: Topic[]) => {
+    const topic_ids: string[] = topics.map(topic => topic.id);
+    
+    const response = await apiHandler<Blog>(`${BLOG_API_URL}/${id}`, {
+        method: 'PATCH',
+        body: JSON.stringify({
+            "title": title,
+            "content": content,
+            "is_draft": true,
+            "topic_ids": topic_ids
+        })
     })
+    return response;
+}
+
+export const updateBlogAndPublish = async (id: string, title: string, content: string, topics: Topic[]) => {
+    const topic_ids: string[] = topics.map(topic => topic.id);
+
+    const response = await apiHandler<Blog>(`${BLOG_API_URL}/${id}`, {
+        method: 'PATCH',
+        body: JSON.stringify({
+            "title": title,
+            "content": content,
+            "is_draft": false,
+            "topic_ids": topic_ids
+        })
+    })
+    return response;
+}
+
+export const deleteBlog = (id: string) => {
+    apiHandler<{ success: boolean }>(`${BLOG_API_URL}/${id}`, {method: 'DELETE'})
+}
 
 export const getAllDraftBlogs = () => apiHandler<Blog[]>(`${BLOG_API_URL}/draft`)
 
