3b00f0278269ff75761dbe8d7d5952848ae615f3
swagatobhaskar <bhaskarswagato@gmail.com>
2025-09-16 01:26:56 +0530

    ADD: some API_routes.

--- blog-ui/lib/constants/constants.ts
+++ blog-ui/lib/constants/constants.ts
@@ -1,4 +1,4 @@
-export const API_URL = process.env.NEXT_PUBLIC_API_URL || 'http://127.0.0.1:8000';
+export const API_URL = process.env.NEXT_PUBLIC_API_URL || 'http://127.0.0.1:8000/api';
 export const BLOG_API_URL = `${API_URL}/blog`;
 export const TOPICS_API_URL = `${API_URL}/topic`;
 export const USER_API_URL = `${API_URL}/user`;
@@ -6,3 +6,4 @@ export const AUTH_API_URL = `${API_URL}/auth`;
 export const AUTH_SIGNUP_API_URL = `${API_URL}/auth/signup`;
 export const AUTH_LOGIN_API_URL = `${API_URL}/auth/login`;
 export const AUTH_LOGOUT_API_URL = `${API_URL}/auth/logout`;
+export const AUTH_REFRESH_TOKEN_API_URL = `${API_URL}/auth/refresh-token`;
