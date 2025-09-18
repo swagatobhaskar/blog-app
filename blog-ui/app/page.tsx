311d7b63aa9ce550abb46ce49c090c4e7edf8831
swagatobhaskar <bhaskarswagato@gmail.com>
2025-09-19 01:12:26 +0530

    EDIT: destructure user from GetLoggedInUser. Future search feature.

--- blog-ui/app/page.tsx
+++ blog-ui/app/page.tsx
@@ -3,9 +3,10 @@ import { BLOG_API_URL } from "@/lib/constants/constants"
 import BlogListItem from "@/components/blog/blogListItem"
 import AdminNav from "@/components/layout/adminNav";
 import { GetLoggedInUser } from "@/lib/server-auth/getLoggedInUser";
+import SearchBlogByTitleOrTopic from "@/components/blog/searchBlogByTitleOrTopic";
 
 export default async function Home() {
-    const user = await GetLoggedInUser();
+    const { user } = await GetLoggedInUser();
     
     const res = await fetch(
         `${BLOG_API_URL}`, {
@@ -21,6 +22,9 @@ export default async function Home() {
     return (
         <div className="w-full lg:w-3/6 mx-auto px-10 mt-5 lg:mt-10">
             { user && <AdminNav /> }
+            {/* Search Feature */}
+            <SearchBlogByTitleOrTopic />
+
             {blogs.length === 0 && <p className="font-light text-2xl">Nothing to see here...</p>}
             {blogs.map((blog) => (
                 <BlogListItem key={blog.id} blog={blog} user={user} />
