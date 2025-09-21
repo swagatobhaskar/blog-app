db2a62ea5cec7fd9e8bbed77254726cd0fcba2ff
swagatobhaskar <bhaskarswagato@gmail.com>
2025-09-22 01:25:21 +0530

    ADD: add the returned cookie to the cookies

--- blog-ui/lib/server-auth/getLoggedInUser.ts
+++ blog-ui/lib/server-auth/getLoggedInUser.ts
@@ -5,6 +5,7 @@ import User from "../types/user";
 import { AUTH_REFRESH_TOKEN_API_URL, USER_API_URL } from "../constants/constants";
 
 export async function GetLoggedInUser(): Promise<{ user: User | null; accessToken?: string }> {
+    console.log("INSIDE GetLoggedInUser()..")
     const cookieStore = await cookies();
 
     let accessToken = cookieStore.get('access_token')?.value;
@@ -13,7 +14,8 @@ export async function GetLoggedInUser(): Promise<{ user: User | null; accessToke
     if (!accessToken && !refreshToken) return { user: null };
 
     async function fetchCurrentUser(accessToken: string) {
-        // console.log("Running in utils/server-auth/GetLoggedInUser.ts")
+        console.log("Running in utils/server-auth/GetLoggedInUser.ts")
+
         return await fetch(`${USER_API_URL}`, {
             method: 'GET',
             headers: {
@@ -26,7 +28,8 @@ export async function GetLoggedInUser(): Promise<{ user: User | null; accessToke
     let resp = accessToken ? await fetchCurrentUser(accessToken) : null;
 
     if (resp?.status === 401 && refreshToken) {
-        // console.log("Fetching for access by refresh @ getLoggedInUser().")
+        console.log("Fetching for access by refresh @ getLoggedInUser().")
+
         const refreshResp = await fetch(`${AUTH_REFRESH_TOKEN_API_URL}`, {
             method: 'POST',
             headers: {
@@ -41,6 +44,14 @@ export async function GetLoggedInUser(): Promise<{ user: User | null; accessToke
             const newAccessToken = setCookieHeader?.match(/access_token=([^;]+)/)?.[1];
             if (newAccessToken) {
                 accessToken = newAccessToken;
+                // Set the new access token as cookie in the server
+                cookieStore.set("access_token", newAccessToken, {
+                    httpOnly: true,
+                    secure: true,
+                    sameSite: "none",
+                    path: "/",
+                });
+            
                 resp = await fetchCurrentUser(accessToken);
             }
         }
