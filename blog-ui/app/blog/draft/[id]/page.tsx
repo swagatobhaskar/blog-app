661e08e5c200bfe7e7fbbdadd0da40b13678e71a
swagatobhaskar <bhaskarswagato@gmail.com>
2025-09-22 01:29:24 +0530

    bug: draft/id API isn't including coookie!

--- blog-ui/app/blog/draft/[id]/page.tsx
+++ blog-ui/app/blog/draft/[id]/page.tsx
@@ -4,22 +4,23 @@ import { BLOG_API_URL } from "@/lib/constants/constants";
 import Blog from "@/lib/types/blog";
 import RenderBlogAndOptionButtons from "@/components/blog/renderBlog";
 import User from "@/lib/types/user";
+import { GetLoggedInUser } from "@/lib/server-auth/getLoggedInUser";
 
-interface DraftBlogPostProps {
-    id: string;
-    user: User | null;
-}
+export default async function DraftBlogItem({params}: {params: Promise<{ id: string }>}) {
+    const { id } = await params;
+    const { user } = await GetLoggedInUser();
 
-export default async function DraftBlogItem({id, user}: DraftBlogPostProps) {
-    // const { id } = params;
+    console.log("id, user: ", id, user);
     const res = await fetch(`${BLOG_API_URL}/draft/${id}`, {
             method: 'GET',
             headers: {'Content-Type': 'application/json',},
-            cache: "default", // better performance if data updates infrequently
+            cache: "no-store", // prevent stale drafts
+            // cache: "default", // better performance if data updates infrequently
         });
-        // Show 404 page if blog is not found
-        if (!res.ok) return notFound();
+    // Show 404 page if blog is not found
+    if (!res.ok) return notFound();
     const draftBlog: Blog = await res.json()
+    console.log("DRAFTBLOG: ", draftBlog)
     
     return (
         <div className="">
