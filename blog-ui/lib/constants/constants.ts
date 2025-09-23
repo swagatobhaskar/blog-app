4f15697584645f30a467cd6420ec31bc7415736c
swagatobhaskar <bhaskarswagato@gmail.com>
2025-09-24 01:15:59 +0530

    ADD: draft API url

--- blog-ui/lib/constants/constants.ts
+++ blog-ui/lib/constants/constants.ts
@@ -1,5 +1,6 @@
 export const API_URL = process.env.NEXT_PUBLIC_API_URL || 'http://127.0.0.1:8000/api';
 export const BLOG_API_URL = `${API_URL}/blog`;
+export const DRAFT_BLOG_API_URL = `${API_URL}/blog/draft`;
 export const TOPICS_API_URL = `${API_URL}/topic`;
 export const USER_API_URL = `${API_URL}/user`;
 export const AUTH_API_URL = `${API_URL}/auth`;
