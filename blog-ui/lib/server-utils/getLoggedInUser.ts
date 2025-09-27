4406673394d9668d3d2f4218bf9950f0d564e42a
swagatobhaskar <bhaskarswagato@gmail.com>
2025-09-28 01:18:53 +0530

    WIP: API as proxy handler, with token refresh.

--- blog-ui/lib/server-utils/getLoggedInUser.ts
+++ blog-ui/lib/server-utils/getLoggedInUser.ts
@@ -1,33 +1,17 @@
 // server component utility function
 
 import User from "../types/user";
-import ServerFetchHandler from "./serverFetchHandler";
+import { ProxyFetchHandler } from "./proxyFetchHandler";
 import { USER_API_URL } from "../constants/constants";
 
 export async function GetLoggedInUser(): Promise<{ user: User | null }> {
     console.log("Inside GetLoggedInUser()..")
 
-    const resp = await ServerFetchHandler({ url: `${USER_API_URL}` })
+    const resp = await ProxyFetchHandler(`${USER_API_URL}`)
+    console.log("GetLoggedInUser Result: ", resp)
 
     if (!resp.ok) return { user: null }
 
     const user: User = await resp.json()
     return { user }
 }
-
-// if (refreshResp.ok) {
-//     // if you want to use the new access token right away
-//     // (e.g., to retry the request), you have to manually extract it from the Set-Cookie header
-//     const setCookieHeader = refreshResp.headers.get("set-cookie");
-//     const newAccessToken = setCookieHeader?.match(/access_token=([^;]+)/)?.[1];
-//     if (newAccessToken) {
-//         accessToken = newAccessToken;
-//         cookieStore.set("access_token", newAccessToken, {
-//             httpOnly: true,
-//             secure: true,
-//             sameSite: "none",
-//             path: "/",
-//         });
-//         resp = await fetchCurrentUser(accessToken);
-//     }
-// }
---
+++ blog-ui/lib/server-utils/proxyFetchHandler.ts
@@ -0,0 +1,25 @@
+// Server component Helper Function
+
+import { cookies } from "next/headers";
+
+export async function ProxyFetchHandler<T = any>(
+    path: string,
+    init?: RequestInit
+): Promise<T> {
+    const cookieHeader = (await cookies()).toString();
+
+    const res = await fetch(`/api/proxy/${path}`, {
+        ...init,
+        headers: {
+            ...(init?.headers || {}),
+            Cookie: cookieHeader
+        },
+        cache: "no-store",
+    });
+
+    if (!res.ok) {
+        throw new Error(`Proxy fetch failed (${res.status}: ${await res.text()})`);
+    }
+
+    return res.json(); // as Promise<T>;
+}
